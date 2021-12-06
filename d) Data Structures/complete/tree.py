15b16163e2ff04e65a984ea6225802b9430bd53f
Paul McSlarrow <74205136+Pmcslarrow@users.noreply.github.com>
2021-12-06 13:48:56 -0800

    Delete d) Data Structures directory

--- d) Data Structures/complete/tree.py
+++
@@ -1,9 +0,0 @@
-#tree.py
-class Node:
-    def __init__(self, value):
-        self.item = value
-        self.left = None
-        self.right = None
-    
-    def get_node(self):
-        return self.item
